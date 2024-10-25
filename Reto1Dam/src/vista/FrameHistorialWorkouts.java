33af1ea949ce4350e93f9211684bcc4870267ba8
in2dm3-a <in2dm3-a@HZ401013.elorrieta.int>
2024-10-25 12:26:15 +0200

    Historial Workouts

--- Reto1Dam/src/vista/FrameHistorialWorkouts.java
+++ Reto1Dam/src/vista/FrameHistorialWorkouts.java
@@ -1,10 +1,19 @@
 package vista;
 
 import java.awt.EventQueue;
-
+import java.awt.Font;
+import java.util.ArrayList;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import modelo.HistoricoWorkouts;
+
 import javax.swing.JButton;
 
 public class FrameHistorialWorkouts extends JFrame {
@@ -12,7 +21,11 @@ public class FrameHistorialWorkouts extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JButton btnAtras;
-	
+	private DefaultTableModel defaultTableModel;
+	private JTable tablaHistorial;
+	private JLabel lblWorkouts;
+	private JButton btnPerfil;
+	private JButton btnSeleccionar;
 
 	/**
 	 * Launch the application.
@@ -41,13 +54,73 @@ public class FrameHistorialWorkouts extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
+		// tabla con datos
+		JScrollPane jScrollPanel = new JScrollPane();
+		jScrollPanel.setBounds(63, 68, 408, 217);
+		contentPane.add(jScrollPanel);
+
+		String columnas[] = { "Nombre", "nº de ejercicios", "Nivel", "Video(URL)" };
+
+		defaultTableModel = new DefaultTableModel(columnas, 0);
+
+		tablaHistorial = new JTable(defaultTableModel);
+		tablaHistorial.setColumnSelectionAllowed(false);
+		tablaHistorial.setCellSelectionEnabled(false);
+		tablaHistorial.setRowSelectionAllowed(true);
+		tablaHistorial.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		tablaHistorial.setAutoCreateRowSorter(true);
+
+		// anulamos la edicion de las celdas
+		tablaHistorial.setDefaultEditor(Object.class, null);
+
+		jScrollPanel.setViewportView(tablaHistorial);
+
+		lblWorkouts = new JLabel("WORKOUTS");
+		lblWorkouts.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblWorkouts.setBounds(10, 11, 99, 14);
+		contentPane.add(lblWorkouts);
+
+		btnPerfil = new JButton("Perfil");
+		btnPerfil.setBounds(435, 9, 89, 23);
+		contentPane.add(btnPerfil);
+
+//				btnHistorial = new JButton("Historial de workouts");
+//				btnHistorial.setBounds(10, 327, 168, 23);
+//				contentPane.add(btnHistorial);
+
+		btnSeleccionar = new JButton("Seleccionar");
+		btnSeleccionar.setBounds(412, 327, 112, 23);
+		contentPane.add(btnSeleccionar);
+
 		btnAtras = new JButton("Atrás");
 		btnAtras.setBounds(10, 11, 89, 23);
 		contentPane.add(btnAtras);
 	}
+	
+	//metodo para insertar los workout en la tabla
+		public void insertarHistoricos() {
+			//limpiar las filas
+			defaultTableModel.setRowCount(0);
 
-	//getters
+			HistoricoWorkouts historicoInstance = new HistoricoWorkouts();
+			ArrayList<HistoricoWorkouts> listaHistoricoWorkouts = historicoInstance.mObtenerHistoricoWorkouts(getName());
+
+			//lo recorremos y añadimos a la tabla
+			for (HistoricoWorkouts HistoricoWorkout : listaHistoricoWorkouts) {
+				Object[] rowData = {
+						HistoricoWorkout.getNombreW(),
+						HistoricoWorkout.getNivel(),
+						HistoricoWorkout.getTiempoTotal().intValue(),
+						HistoricoWorkout.getTiempoPrevisto().intValue(),
+						HistoricoWorkout.getFecha()
+				};
+
+				defaultTableModel.addRow(rowData);
+			}
+		}
+
+	// getters
 	public JPanel getContentPane() {
 		return contentPane;
 	}
@@ -56,6 +129,4 @@ public class FrameHistorialWorkouts extends JFrame {
 		return btnAtras;
 	}
 
-	
-	
 }
